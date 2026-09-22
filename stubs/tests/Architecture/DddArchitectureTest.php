f7879c7ac6c2936a62da36ac1d7c3d4d2a35b544
Maio Barbero <maiowebdesign@gmail.com>
2026-09-22 14:54:45 +0200

    Skip test if folders are missing

--- stubs/tests/Architecture/DddArchitectureTest.php
+++ stubs/tests/Architecture/DddArchitectureTest.php
@@ -1,6 +1,10 @@
 <?php
 
 arch('domain does not depend on outer application layers')
+    ->skip(
+        fn () => glob(dirname(__DIR__, 3).'/app/Domain', GLOB_ONLYDIR) === [],
+        'Skipping, Domain folder doesn\'t exist.',
+    )
     ->expect('App\Domain')
     ->not->toUse([
         'App\Application',
@@ -14,10 +18,18 @@ arch('domain does not depend on outer application layers')
     ]);
 
 arch('application does not depend on infrastructure')
+    ->skip(
+        fn () => glob(dirname(__DIR__, 3).'/app/Application', GLOB_ONLYDIR) === [],
+        'Skipping, Application folder doesn\'t exist.',
+    )
     ->expect('App\Application')
     ->not->toUse('App\Infrastructure');
 
 arch('application does not depend on delivery mechanisms')
+    ->skip(
+        fn () => glob(dirname(__DIR__, 3).'/app/Application', GLOB_ONLYDIR) === [],
+        'Skipping, Application folder doesn\'t exist.',
+    )
     ->expect('App\Application')
     ->not->toUse([
         'App\Http',
@@ -27,11 +39,19 @@ arch('application does not depend on delivery mechanisms')
     ]);
 
 arch('actions expose handle')
+    ->skip(
+        fn () => glob(dirname(__DIR__, 3).'/app/Application/*/Actions', GLOB_ONLYDIR) === [],
+        'Skipping, Application\Actions folders doesn\'t exist.',
+    )
     ->expect('App\Application\*\Actions')
     ->classes()
     ->toHaveMethod('handle');
 
 arch('application data uses Data suffix')
+    ->skip(
+        fn () => glob(dirname(__DIR__, 3).'/app/Application/*/Data', GLOB_ONLYDIR) === [],
+        'Skipping, Application\Data doesn\'t exist.',
+    )
     ->expect('App\Application\*\Data')
     ->classes()
     ->toHaveSuffix('Data');
